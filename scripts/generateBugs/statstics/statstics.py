435269a13d362649b7da47d54805affd09f6c2e7
docker_macos <universetraveller@github.com>
2023-07-26 11:34:33 -0700

    statstics for Projects

--- scripts/generateBugs/statstics/statstics.py
+++ scripts/generateBugs/statstics/statstics.py
@@ -20,9 +20,9 @@ def is_all(p, l):
             return False
     return True
 def get_data():
-    paths = glob.glob('./working/*/log')
+    paths = glob.glob('../working/*/log')
     for i in paths:
-        name = i.replace('./working/', '').replace('/log', '')
+        name = i.replace('../working/', '').replace('/log', '')
         with open(i, 'r') as f:
             f = f.read().splitlines()
         if new_bugs_num(f) > -1:
@@ -81,5 +81,54 @@ def parse_all_statstic1():
     locale.close()
     print('statstics1 ends')
     return 0
+def parse_all_statstic1_name(pname):
+    locale = open(f'./statstics1_{pname}.csv', 'a')
+    locale.write('bug_id, status, num_new_bugs, hunks_new_bugs\n')
+    isolated = 0
+    divisible = 0
+    indivisible = 0
+    for name in data:
+        if not pname in name:
+            continue
+        tp = data[name]
+        name = name.replace(pname+'_', '')
+        log = tp[0]
+        nbn = new_bugs_num(log)
+        if nbn == -1:
+            locale.write(f'{name}, {NO_DATA}, 0, NONE\n')
+            alert(f'{name} no ending')
+        elif nbn == 0:
+            locale.write(f'{name}, {INVALID_DIVISIBLE_BUG}, {NO_DATA}, NONE\n')
+            divisible += 1
+        else:
+            bgdict = tp[1]
+            bl = new_bugs_from_bgdict(bgdict)
+            if not len(bl):
+                alert(f'Empty new BUGS at {name}')
+                continue
+            if nbn == 1:
+                if is_all('1', bl[0]):
+                    locale.write(f'{name}, {INDIVISIBLE}, 0, {len(bl[0])}\n')
+                    indivisible += 1
+                else:
+                    locale.write(f'{name}, {VALID_DIVISIBLE_BUG}, 1, {len(bl[0])}\n')
+                    divisible += 1
+                    isolated += 1
+            else:
+                hs = ''
+                for i in bl:
+                    hs += ('{}.'.format(i.count('1')))
+                locale.write(f'{name}, {VALID_DIVISIBLE_BUG}, {len(bl)}, {hs}\n')
+                divisible += 1
+                isolated += len(bl)
+    locale.write(f'STATSTICS, Divisible={divisible}, Indivisible={indivisible}, Isolated={isolated}\n')
+    locale.close()
+    print(f'statstics1_{pname} ends')
+    return 0
+def parse_all_statstic1_all_proj():
+    assert not get_data()
+    projs = ['Chart', 'Lang', 'Math', 'Time', 'Closure', 'Mockito']
+    for i in projs:
+        assert not parse_all_statstic1_name(i)
 if __name__ == '__main__':
-    parse_all_statstic1()
+    parse_all_statstic1_all_proj()
