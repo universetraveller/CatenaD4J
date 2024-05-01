c287506699a38d0effb3f2bfa5151abe0e8897c9
universetraveller <universetraveller@github.com>
2024-05-01 04:50:35 -0700

    Fix bugs in scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
-curl https://raw.githubusercontent.com/universetraveller/CatenaD4J/main/Dockerfile -o Dockerfile.main
-docker build -t catena4j:main -f ./Dockerfile.main .
+if [ -z "$(docker images -q catena4j:main 2> /dev/null)" ]; then
+        curl https://raw.githubusercontent.com/universetraveller/CatenaD4J/main/Dockerfile -o Dockerfile.main
+        docker build -t catena4j:main -f ./Dockerfile.main .
+fi
+if [ ! -f ./Dockerfile.experiments ] ; then
+        curl https://raw.githubusercontent.com/universetraveller/CatenaD4J/main/scripts/Dockerfile.experiments -o Dockerfile.experiments
+fi
 docker build -t catena4j:experiments -f ./Dockerfile.experiments .
