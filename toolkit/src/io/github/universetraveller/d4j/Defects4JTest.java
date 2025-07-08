19054a723697ed33901e73333ba5b509ba2b8bfb
universetraveller <93870078+universetraveller@users.noreply.github.com>
2025-07-08 06:34:02 -0700

    Change internal data structure to store test methods from HashMap to LinkedHashMap to keep the method order

--- toolkit/src/io/github/universetraveller/d4j/Defects4JTest.java
+++ toolkit/src/io/github/universetraveller/d4j/Defects4JTest.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -77,7 +77,8 @@ public class Defects4JTest extends Defects4JExport {
     public Defects4JTest(String projectBuildFile) throws MalformedURLException {
         super(projectBuildFile);
 
-        methods = new HashMap<>();
+        // the order of tests matters in some projects like Time
+        methods = new LinkedHashMap<>();
 
         String[] pathElements = getTestClasspath().split(":");
 
