a83b04ffb2c99d63b2f1d11cd3f5b49bb7fa5d33
universetraveller <93870078+universetraveller@users.noreply.github.com>
2025-12-21 02:23:41 -0800

    Delete toolkit/compile.sh

--- toolkit/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Updated to use Gradle wrapper instead of javac
-# Usage: ./compile.sh
-# Note: The defects4j path parameter is no longer needed as dependencies are managed by Gradle
-
-./gradlew clean build --no-daemon
-
