9f31f5fc401918fd60b98a6e91b333b1823321d5
universetraveller <93870078+universetraveller@users.noreply.github.com>
2025-12-24 22:29:47 -0800

    Update patches

--- internal/ProjectManager/FileTypes.py
+++ internal/ProjectManager/FileTypes.py
@@ -88,13 +88,16 @@ class EditCacheFile:
         self.encoding=encoding
         self.read()
     def read(self):
-        if not os.path.exists(self.filepath):
-            self.content = [EditCacheLine(1, ''),]
+        if os.path.exists(self.filepath):
+            with open(self.filepath, 'r', encoding=self.encoding) as f:
+                # with return characters
+                # for line commit would add suffix after content
+                f = f.readlines()
+        else:
+            f = []
+        if not f:
+            self.content = [EditCacheLine(1, '')]
             return
-        with open(self.filepath, 'r', encoding=self.encoding) as f:
-            # with return characters
-            # for line commit would add suffix after content
-            f = f.readlines()
         for idx in range(len(f)):
             self.content.append(EditCacheLine(idx + 1, f[idx]))
     def toString(self, keeps_line_no:bool=False):
